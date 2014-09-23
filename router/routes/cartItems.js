1f64462ca81695b857450bd26640c2f0a12ab0c5
liyanzi <843624084@qq.com>
2014-09-24 00:26:24 +0800

    add get in cartItems

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -4,6 +4,14 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+var _ = require('lodash');
+
+router.get('/', function(req, res){
+  client.get('cartItems',function(err,data){
+    res.send(data);
+  });
+});
+
 
 
 
