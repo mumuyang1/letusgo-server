1c7e1957561da9f318b8e618089926afd596e3d5
liyanzi <843624084@qq.com>
2014-09-24 12:03:49 +0800

    add cartItems post

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
-var _ = require('lodash');
+ var _ = require('lodash');
 
 router.get('/', function(req, res){
   client.get('cartItems',function(err,data){
@@ -12,7 +12,25 @@ router.get('/', function(req, res){
   });
 });
 
+router.post('/:id', function(req, res){
 
+  client.get('allProducts',function(err,data){
+
+
+    var products = JSON.parse(data);
+
+    var result = _.find(products,{'id': parseInt(req.params.id)});
+
+    var cartItems = [];
+    var cartItem = {'item':result,'count' : 1 };
+
+    cartItems.push(cartItem);
+
+    client.set('cartItems',JSON.stringify(cartItems),function(err, data){
+     res.send(data);
+   });
+  });
+});
 
 
 module.exports = router;
