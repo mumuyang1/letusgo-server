2790acea029b4a648076b6c3bc5e4c3ebeed6d07
liyanzi <843624084@qq.com>
2014-10-05 18:43:54 +0800

    modify item delete method

--- router/routes/items.js
+++ router/routes/items.js
@@ -6,57 +6,6 @@ var client = redis.createClient();
 
 var _ = require('lodash');
 
-router.get('/', function(req, res) {
-
-  client.get('allProducts',function(err,data){
-
-    res.send(data);
-  });
-});
-
-
-router.post('/', function(req, res){
-
-    var allProductsArray = [
-                    {id:1,barcode:'ITEM000001',categoryId:1,name:'苹果',price:'3.00',unit:'斤'},
-                    {id:2,barcode:'ITEM000002',categoryId:1,name:'香蕉',price:'3.50',unit:'斤'},
-                    {id:3,barcode:'ITEM000003',categoryId:1,name:'菠萝',price:'4.00',unit:'个'},
-                    {id:4,barcode:'ITEM000004',categoryId:2,name:'雪碧',price:'3.00',unit:'瓶'},
-                    {id:5,barcode:'ITEM000005',categoryId:2,name:'可口可乐',price:'3.00',unit:'瓶'},
-                    {id:6,barcode:'ITEM000006',categoryId:3,name:'公牛插座',price:'10.00',unit:'个'},
-                    {id:7,barcode:'ITEM000007',categoryId:3,name:'水杯',price:'16.00',unit:'个'},
-                    {id:8,barcode:'ITEM000008',categoryId:4,name:'钻石项链',price:'160000.00',unit:'个'},
-                    {id:9,barcode:'ITEM000009',categoryId:4,name:'翡翠手镯',price:'200.00',unit:'个'}
-                  ];
-
-      var allProducts = req.param('allProducts')|| allProductsArray;
-
-     client.set('allProducts',JSON.stringify(allProducts),function(err, data){
-      res.send(data);
-    });
-  });
-
-  router.delete('/:id', function(req, res) {
-
-      var id = req.params.id;
-      client.get('allProducts',function(err,data){
-
-        var allProducts = JSON.parse(data);
-        _.forEach(allProducts,function(product){
-          if(product.barcode === id){
-
-              allProducts = _.without(allProducts,product);
-
-              client.set('allProducts',JSON.stringify(allProducts),function(err, allProducts){
-                res.send(allProducts);
-              });
-           }
-
-        });
-     });
-  });
-
-
   function add(allProducts,newProduct){
     var lastBarcode = allProducts[allProducts.length - 1].barcode;
     var i = +lastBarcode.substring(9,lastBarcode.length) + 1;
@@ -66,6 +15,72 @@ router.post('/', function(req, res){
     return allProducts;
   }
 
+  function modify(allProducts,id,item){
+    _.forEach(allProducts,function(product){
+
+        if(product.id === id){
+            product.name =  item.name;
+            product.unit =  item.unit;
+            product.price =  item.price;
+            product.categoryId =  item.categoryId;
+        }
+    });
+    return allProducts;
+  }
+
+  function deleteItem(allProducts,id){
+    _.forEach(allProducts,function(product){
+
+      if(product.barcode === id){
+          allProducts = _.without(allProducts,product);
+       }
+    });
+    return allProducts;
+  }
+
+
+  router.get('/', function(req, res) {
+
+    client.get('allProducts',function(err,data){
+
+      res.send(data);
+    });
+  });
+
+
+  router.post('/', function(req, res){
+
+      var allProductsArray = [
+                      {id:1,barcode:'ITEM000001',categoryId:1,name:'苹果',price:'3.00',unit:'斤'},
+                      {id:2,barcode:'ITEM000002',categoryId:1,name:'香蕉',price:'3.50',unit:'斤'},
+                      {id:3,barcode:'ITEM000003',categoryId:1,name:'菠萝',price:'4.00',unit:'个'},
+                      {id:4,barcode:'ITEM000004',categoryId:2,name:'雪碧',price:'3.00',unit:'瓶'},
+                      {id:5,barcode:'ITEM000005',categoryId:2,name:'可口可乐',price:'3.00',unit:'瓶'},
+                      {id:6,barcode:'ITEM000006',categoryId:3,name:'公牛插座',price:'10.00',unit:'个'},
+                      {id:7,barcode:'ITEM000007',categoryId:3,name:'水杯',price:'16.00',unit:'个'},
+                      {id:8,barcode:'ITEM000008',categoryId:4,name:'钻石项链',price:'160000.00',unit:'个'},
+                      {id:9,barcode:'ITEM000009',categoryId:4,name:'翡翠手镯',price:'200.00',unit:'个'}
+                    ];
+
+        var allProducts = req.param('allProducts')|| allProductsArray;
+
+       client.set('allProducts',JSON.stringify(allProducts),function(err, data){
+        res.send(data);
+      });
+    });
+
+  router.delete('/:id', function(req, res) {
+
+      var id = req.params.id;
+      client.get('allProducts',function(err,data){
+
+        var allProducts = deleteItem(JSON.parse(data),id);
+        client.set('allProducts',JSON.stringify(allProducts),function(err, allProducts){
+          res.send(allProducts);
+        });
+
+     });
+  });
 
   router.post('/:id', function(req, res) {
 
@@ -81,19 +96,6 @@ router.post('/', function(req, res){
           });
     });
   });
-
-  function modify(allProducts,id,item){
-    _.forEach(allProducts,function(product){
-
-        if(product.id === id){
-            product.name =  item.name;
-            product.unit =  item.unit;
-            product.price =  item.price;
-            product.categoryId =  item.categoryId;
-        }
-    });
-    return allProducts;
-  }
 
   router.put('/:id', function(req, res) {
 
